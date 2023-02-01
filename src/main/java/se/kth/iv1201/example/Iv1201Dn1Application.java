18de29914a7be6f5567d1f1935e18a218aac84be
Utbildadninja <ndam@kth.se>
2023-02-01 15:58:37 +0100

    Initial commit

---
+++ src/main/java/se/kth/iv1201/example/Iv1201Dn1Application.java
@@ -0,0 +1,13 @@
+package se.kth.iv1201.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iv1201Dn1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Iv1201Dn1Application.class, args);
+	}
+
+}
---
+++ src/test/java/se/kth/iv1201/example/Iv1201Dn1ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.kth.iv1201.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Iv1201Dn1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
