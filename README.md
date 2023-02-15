29ad9cae1ca94bafc2a55e3a81ff7e57bfb5d5fb
Utbildadninja <ndam@kth.se>
2023-02-15 17:59:52 +0100

    Able to login with mock account

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # iv1201
 Project work for KTH course IV1201, recruitment app for amusement park
+
+How to run with IntelliJ:
+Download
+Reload Maven project
+Build
+Run
--- src/main/java/se/kth/iv1201/presentation/person/PersonController.java
+++ src/main/java/se/kth/iv1201/presentation/person/PersonController.java
@@ -21,6 +21,8 @@ public class PersonController {
     static final String DEFAULT_PAGE_URL = "/";
     static final String LOGIN_PAGE_URL = "login";
     static final String CREATE_USER_PAGE_URL = "createUser";
+    static final String HOME_PAGE_URL = "home";
+    static final String HELLO_PAGE_URL = "hello";
 
     @Autowired
     private RecruitmentService service;
@@ -33,7 +35,25 @@ public class PersonController {
      */
     @GetMapping("/")
     public String showDefaultView() {
-        return "redirect:" + LOGIN_PAGE_URL;
+        return "redirect:" + HOME_PAGE_URL;
+    }
+
+    /**
+     * Redirection to home page.
+     * @return Redirect to home page.
+     */
+    @GetMapping("/home")
+    public String showHomeView() {
+        return HOME_PAGE_URL;
+    }
+
+    /**
+     * Redirection to hello page.
+     * @return Redirect to hello page.
+     */
+    @GetMapping("/hello")
+    public String showHelloView() {
+        return HELLO_PAGE_URL;
     }
 
     /**
@@ -55,19 +75,20 @@ public class PersonController {
      */
     @PostMapping("/" + LOGIN_PAGE_URL)
     public String login(@ModelAttribute(name="loginForm") LoginForm loginForm, Model m) {
-        String username = loginForm.getUsername();
-        String password = loginForm.getPassword();
-        // make db call here
-        try{
-            service.loginPerson(loginForm.getUsername(), loginForm.getPassword());
-        } catch(IllegalDatabaseAccessException ide){
-            m.addAttribute("error", ide.getMessage());
-            return LOGIN_PAGE_URL;
-        }
-
-        m.addAttribute("username", username);
-        m.addAttribute("password", password);
-        return "application";
+//        String username = loginForm.getUsername();
+//        String password = loginForm.getPassword();
+//        // make db call here
+//        try{
+//            service.loginPerson(loginForm.getUsername(), loginForm.getPassword());
+//        } catch(IllegalDatabaseAccessException ide){
+//            m.addAttribute("error", ide.getMessage());
+//            return LOGIN_PAGE_URL;
+//        }
+//
+//        m.addAttribute("username", username);
+//        m.addAttribute("password", password);
+//        return "application";
+        return "home";
     }
 
     /**
