2ab5cefa60c17a095b82f9858f1ba05c31435012
Utbildadninja <ndam@kth.se>
2023-03-01 10:50:17 +0100

    fix: Recruiters should not see homeApplicant

--- src/main/java/se/kth/iv1201/config/WebSecurityConfig.java
+++ src/main/java/se/kth/iv1201/config/WebSecurityConfig.java
@@ -40,6 +40,7 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/", "/" + LOGIN_PAGE_URL, "/" + CREATE_USER_PAGE_URL).permitAll()
                         .requestMatchers("/" + HOME_RECRUITER_URL,"/"+ APPLICATIONS_URL).hasAuthority(ROLE_RECRUITER)
+                        .requestMatchers("/" + HOME_APPLICANT_URL).hasAuthority(ROLE_APPLICANT)
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
