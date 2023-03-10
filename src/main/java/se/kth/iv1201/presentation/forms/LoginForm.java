6f669c40744a2705899ee7f26e69ee5495eb2d36
Utbildadninja <ndam@kth.se>
2023-03-10 12:04:35 +0100

    hotfix: Loginform

--- src/main/java/se/kth/iv1201/presentation/forms/LoginForm.java
+++ src/main/java/se/kth/iv1201/presentation/forms/LoginForm.java
@@ -7,27 +7,27 @@ import jakarta.validation.constraints.NotBlank;
  * A form bean for the login form.
  */
 public class LoginForm {
-//    private final String notBlankMessage = "Please write something.";
-//
-//    @NotBlank(message = notBlankMessage)
-//    private String username;
-//
-//    @NotBlank(message = notBlankMessage)
-//    private String password;
-//
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
+    private final String notBlankMessage = "Please write something.";
+
+    @NotBlank(message = notBlankMessage)
+    private String username;
+
+    @NotBlank(message = notBlankMessage)
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
